c849a90341f71efa5b9393f0489a7babcc8d29ca
Amani215 <amanibrik5@gmail.com>
2022-08-05 18:34:48 +0200

    Added: pyJWT dependency

--- api/model/__init__.py
+++ api/model/__init__.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from app import app
 
 # Setup db
-
+app.config['SECRET_KEY']='903900a2865fbcc43f5752851729f6ef'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://db:pass@db:5432/db'
 db = SQLAlchemy(app)
--- api/route/auth.py
+++ api/route/auth.py
@@ -1,14 +1,40 @@
+from functools import wraps
 from flask import Blueprint
 from flask import Blueprint, jsonify, request
+import jwt
+from model.user import User
 import service.user as user_service
+from app import app
 
 auth_api = Blueprint('auth_api',__name__)
 
+  
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+
+      token = None
+
+      if 'x-access-tokens' in request.headers:
+          token = request.headers['x-access-tokens']
+
+      if not token:
+          return jsonify({'message': 'a valid token is missing'})
+
+      try:
+          data = jwt.decode(token, app.config['SECRET_KEY'])
+          current_user = User.query.filter_by(public_id=data['public_id']).first()
+      except:
+          return jsonify({'message': 'token is invalid'})
+
+      return f(current_user, *args, **kwargs)
+    return decorator
+
 @auth_api.route('/api/auth/', methods=['POST'])
 def authenticate():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    result = user_service.verify_user(username=username, password=password)
+    auth_data = request.authorization
+    
+    result = user_service.verify_user(username=auth_data.username, password=auth_data.password)
     if(result == True):
         return "Authenticated :)"
     else:
