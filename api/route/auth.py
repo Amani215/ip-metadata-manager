0ccdfa77814c1b18b64f4ec69acc448cb1b1a02a
Amani215 <amanibrik5@gmail.com>
2022-08-06 18:53:58 +0200

    Added: token required decorator

--- api/route/auth.py
+++ api/route/auth.py
@@ -2,6 +2,7 @@ from functools import wraps
 from flask import Blueprint
 from flask import Blueprint, jsonify, request
 import jwt
+from schema.token import TokenSchema
 from model.user import User
 import service.auth as auth_service
 from app import app
@@ -10,24 +11,22 @@ auth_api = Blueprint('auth_api',__name__)
 
   
 def token_required(f):
-    @wraps(f)
-    def decorator(*args, **kwargs):
-
+    # @wraps(f)
+    def decorator():
       token = None
 
-      if 'x-access-tokens' in request.headers:
-          token = request.headers['x-access-tokens']
+      if 'Authorization' in request.headers:
+          token = request.headers['Authorization']
 
       if not token:
           return jsonify({'message': 'a valid token is missing'})
 
-      try:
-          data = jwt.decode(token, app.config['SECRET_KEY'])
-          current_user = User.query.filter_by(public_id=data['public_id']).first()
-      except:
-          return jsonify({'message': 'token is invalid'})
+    #   try:
+    #       current_user = auth_service.get_authorized_user(token)
+    #   except:
+    #       return jsonify({'message': 'token is invalid'})
 
-      return f(current_user, *args, **kwargs)
+      return f()
     return decorator
 
 @auth_api.route('/api/auth/', methods=['POST'])
@@ -37,6 +36,7 @@ def authenticate():
     return auth_service.authenticate_user(username=username, password=password)
 
 @auth_api.route('/api/auth/', methods=['GET'])
+@token_required
 def get_authorized_user():
     token:str  = request.headers.get('Authorization',type=str)
     return auth_service.get_authorized_user(token)
