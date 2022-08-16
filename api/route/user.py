ebb0049c8517fb0107ce8a993fab110e0fd29ad3
Amani215 <amanibrik5@gmail.com>
2022-08-16 15:21:04 +0200

    Changed: using APIBlueprints instead

--- api/route/user.py
+++ api/route/user.py
@@ -1,21 +1,34 @@
-from flask import Blueprint, jsonify, request
+from flask import jsonify, request
 from app import app
 import service.user as user_service
+from flask_openapi3 import APIBlueprint, Tag
 
-user_api = Blueprint('user_api',__name__)
+user_api = APIBlueprint('user_api',__name__)
+
+user_tag = Tag(name="user", description="User requests")
 
 @user_api.route('/api/user/', methods = ['GET'])
-@app.get('/api/user')
+@app.get('/api/user', tags=[user_tag])
 def get_users() -> str:
+
     """Get all users
     
     Returns all the users in the database
     """
+
     users = user_service.get_users()
     return jsonify([i.serialize for i in users])
 
+
 @user_api.route('/api/user/', methods=['POST'])
+@app.post('/api/user', tags=[user_tag])
 def add_user():
+    """Add a new user
+    
+    Takes a username and a password and returns the new user ID. 
+    If the username already exists the user is not added.
+    """
+
     username = request.form.get('username')
     password = request.form.get('password')
     user_response = user_service.create_user(username=username, password=password)
