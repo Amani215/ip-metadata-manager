b02fc47ac605522dca6708b96d515fe7b54d9a79
Amani215 <amanibrik5@gmail.com>
2022-08-17 23:51:20 +0200

    Refactored: database initialization

--- api/app.py
+++ api/app.py
@@ -6,30 +6,19 @@ app=OpenAPI(__name__, info = info)
 import config
 
 # Create tables
-#from model import db
-
-#####
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('postgresql://db:pass@db:5432/db', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
-
+from model import Base, engine, db_session
 from model.user import User
+
 #Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
+# close session at shutdown
 @app.teardown_appcontext
 def shutdown_session(exception=None):
-    db_session.remove()
- ########
+   db_session.remove()
+
 # db.drop_all()
-#db.create_all()
+# db.create_all()
 
 #  routing
 from route.user import user_api
