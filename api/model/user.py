7d362a2e8cd69d5d358a830b67d550df7ebbb8ee
Amani215 <amanibrik5@gmail.com>
2022-08-17 23:40:14 +0200

    Changed: SQLAlchemy to declarative_base

--- api/model/user.py
+++ api/model/user.py
@@ -1,9 +1,11 @@
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Column, String
-from app import db
+from app import Base
 import uuid
 
-class User(db.Model):
+class User(Base):
+  __tablename__ = 'User'
+
   id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
   username = Column(String(80), unique=True, nullable=False)
   password = Column(String(128), nullable=False)
